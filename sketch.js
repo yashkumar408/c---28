61143b7d18ed7778161d7700b44a2b36c04d44a7
yashkumar408 <85298771+yashkumar408@users.noreply.github.com>
2021-08-02 09:45:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,72 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint = Matter.Constraint;
+var dustbinObj, paperObject,groundObject,slingshot;
+var world;
+
+
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+
+	engine = Engine.create();
+	world = engine.world;
+	
+	paperObject=new paper(200,450,70);
+	groundObject=new ground(width/2,670,width,20);
+	dustbinObj=new dustbin(1200,650);
+
+	slingshot = new SlingShot(paperObject.body,{x:200, y: 200});
+	
+	
+
+	var render = Render.create({
+	  element: document.body,
+	  engine: engine,
+	  options: {
+	    width: 1600,
+	    height: 700,
+	    wireframes: false
+	  }
+	});
+
+	Engine.run(engine);
+	Render.run(render);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(230);
+ 
+  
+  paperObject.display();
+  groundObject.display();
+  dustbinObj.display();
+
+  slingshot.display();    
+
+}
+
+function mouseDragged(){
+    Matter.Body.setPosition(paperObject.body, {x: mouseX, y: mouseY});
+}
+
+function mouseReleased(){
+   slingshot.fly();
+}
+
+
+
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+
+	  Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:130,y:-145})
+  
+	}
+}
